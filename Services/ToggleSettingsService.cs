6bbe037ec17b6a894ec4cdc579263993ae0d6b0c
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-04-26 15:13:26 -0400

    Update ToggleSettingsService.cs

--- Services/ToggleSettingsService.cs
+++ Services/ToggleSettingsService.cs
@@ -7,34 +7,26 @@ namespace BlazorChatToolKit.Services
 
         public class Settings
         {
-            public bool ChatBinary { get; set; } = false;       
+            public bool ChatBinary { get; set; } = false; 
+            public bool Model { get; set; } = false;
+            public bool Prompt { get; set; } = false;
             public bool Interactive { get; set; } = false;
             public bool InteractiveStart { get; set; } = false;
-
-            public bool ReversePrompt { get; set; } = false;
-            public bool Color { get; set; } = false;    
-
-            public bool Prompt { get; set; } = false;
-
+            public bool Color { get; set; } = false;   
             public bool File { get; set; } = false;
             public bool Seed { get; set; } = false;
             public bool Threads { get; set; } = false;
             public bool NPredict { get; set; } = false;
             public bool TopK { get; set; } = false;
             public bool TopP { get; set; } = false;
-
             public bool RepeatLastN { get; set; } = false;
-
             public bool RepeatPenalty { get; set; } = false;
-
             public bool CtxSize { get; set; } = false;
-
-            public bool Temp { get; set; } = false;
-
-            public bool BatchSize { get; set; } = false;
-            public bool Model { get; set; } = false;
             public bool ContextMemory { get; set; } = false;
-
+            public bool Temp { get; set; } = false;
+            public bool BatchSize { get; set; } = false;
+            public bool ReversePrompt { get; set; } = false;
+            
             // Add other properties here
         }
     }
